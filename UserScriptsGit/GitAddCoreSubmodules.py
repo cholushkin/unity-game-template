e7f4194aec5ac140bc5814da2749c621033b134c
max <cholushkin@gmail.com>
2026-05-25 15:30:00 +0300

    chores: add dev-conventions

--- UserScriptsGit/GitAddCoreSubmodules.py
+++ UserScriptsGit/GitAddCoreSubmodules.py
@@ -4,10 +4,6 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../ScriptUtils")))
 from Core.GitSubmoduleManager.GitSubmoduleManager import GitSubmoduleManager
 
-
-GitSubmoduleManager().add_git_submodule(
-    "https://github.com/cholushkin/coding-convention.git", 
-    "CodingConvention")
     
 GitSubmoduleManager().add_git_submodule(
     "https://github.com/cholushkin/gamelib.git", 
